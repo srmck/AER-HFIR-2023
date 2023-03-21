281a837a07b75679e336770699c35f2b243e57cf
Sam <87244866+srmck@users.noreply.github.com>
2023-03-21 18:02:27 -0400

    Finished 2d scan
    Need to do cross scan, and final working principle scan over MWPs and CG currents for some harmonic. Also try 3mm mask

--- AER_HFIR_2023.c
+++ AER_HFIR_2023.c
@@ -2,7 +2,7 @@
  * Format:     ANSI C source code
  * Creator:    McStas <http://www.mcstas.org>
  * Instrument: AER_HFIR_2023.instr (AER_HFIR_2023)
- * Date:       Tue Mar 21 15:55:47 2023
+ * Date:       Tue Mar 21 17:46:33 2023
  * File:       AER_HFIR_2023.c
  * CFLAGS=
  */
@@ -6129,12 +6129,12 @@ struct _instrument_struct *instrument = & _instrument_var;
 
 int numipar = 9;
 struct mcinputtable_struct mcinputtable[] = {
-  "delta_lambda", &(_instrument_var._parameters.delta_lambda), instr_type_double, "0.01375", 
+  "delta_lambda", &(_instrument_var._parameters.delta_lambda), instr_type_double, "1", 
   "low_count", &(_instrument_var._parameters.low_count), instr_type_int, "1", 
   "all_off", &(_instrument_var._parameters.all_off), instr_type_int, "0", 
-  "cur1", &(_instrument_var._parameters.cur1), instr_type_double, "6.10401", 
-  "cur2", &(_instrument_var._parameters.cur2), instr_type_double, "-6.10401", 
-  "BCG", &(_instrument_var._parameters.BCG), instr_type_double, "25.1627", 
+  "cur1", &(_instrument_var._parameters.cur1), instr_type_double, "6.40401", 
+  "cur2", &(_instrument_var._parameters.cur2), instr_type_double, "-6.40401", 
+  "BCG", &(_instrument_var._parameters.BCG), instr_type_double, "0", 
   "polx", &(_instrument_var._parameters.polx), instr_type_double, "1", 
   "poly", &(_instrument_var._parameters.poly), instr_type_double, "0", 
   "polz", &(_instrument_var._parameters.polz), instr_type_double, "0", 
